09e0cbf9b21a1f5d15815c32aa0a00386188000f
Wesley Luiz <leoncral2@gmail.com>
2019-10-23 20:02:16 -0500

    Iniciando um projeto em Flutter

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Crie belas UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Crie belas UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                'Crie belas UIs',
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
